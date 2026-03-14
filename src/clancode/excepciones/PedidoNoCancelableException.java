0b2a3f2d937304ff60ba7ec9c2179a597014169d
Lara Cordero <lcorderog@uoc.edu>
2026-03-14 12:22:39 +0100

    refactor: Añadir Javadoc específico y constructor con causa encadenada a excepciones

--- src/clancode/excepciones/PedidoNoCancelableException.java
+++ src/clancode/excepciones/PedidoNoCancelableException.java
@@ -1,7 +1,32 @@
 package clancode.excepciones;
 
+/**
+ * Excepción lanzada cuando se intenta cancelar un pedido
+ * que ya ha sido enviado o cuyo plazo de cancelación ha expirado.
+ *
+ * Un pedido es cancelable solo si está pendiente de envío
+ * y aún no ha transcurrido su tiempo de preparación.
+ *
+ * Casos de uso:
+ * - Intento de eliminar pedido con estado "Enviado"
+ * - Intento de eliminar pedido cuyo tiempo de preparación ya expiró
+ *
+ * @author clancode
+ * @version 1.0
+ */
 public class PedidoNoCancelableException extends Exception {
+
     public PedidoNoCancelableException(String mensaje) {
         super(mensaje);
+    }
+
+    /**
+     * Constructor con causa encadenada.
+     *
+     * @param mensaje descripción del error
+     * @param causa la excepción original
+     */
+    public PedidoNoCancelableException(String mensaje, Throwable causa) {
+        super(mensaje, causa);
     }
 }
